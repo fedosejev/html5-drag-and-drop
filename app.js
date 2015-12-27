32194c402f304fd3ad804cec66e411829e0640e4
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-12-27 21:40:22 +0000

    Update

--- app.js
+++ app.js
@@ -1,69 +1,73 @@
-// Read: http://www.html5rocks.com/en/tutorials/dnd/basics/
+(function () {
 
-document.addEventListener('DOMContentLoaded', function handleDOMContentLoaded() {
-  var dropZoneElement = document.querySelector('[data-drop-zone]');
-  dropZoneElement.addEventListener('dragover', handleDragOver, false);
-  dropZoneElement.addEventListener('dragleave', handleDragLeave, false);
-  dropZoneElement.addEventListener('drop', handleDrop, false);
-  
-  var draggableElements = document.querySelectorAll('[draggable]');
-  
-  // Not [].forEach.call();
-  Array.prototype.forEach.call(draggableElements, function (element) {
-    element.addEventListener('dragstart', handleDragStart, false);
-    element.addEventListener('dragenter', handleDragEnter, false);
-    element.addEventListener('dragend', handleDragEnd, false);
+  // Read: http://www.html5rocks.com/en/tutorials/dnd/basics/
+
+  document.addEventListener('DOMContentLoaded', function handleDOMContentLoaded() {
+    var dropZoneElement = document.querySelector('[data-drop-zone]');
+    dropZoneElement.addEventListener('dragover', handleDragOver, false);
+    dropZoneElement.addEventListener('dragleave', handleDragLeave, false);
+    dropZoneElement.addEventListener('drop', handleDrop, false);
+
+    var draggableElements = document.querySelectorAll('[draggable]');
+
+    // Not [].forEach.call();
+    Array.prototype.forEach.call(draggableElements, function (element) {
+      element.addEventListener('dragstart', handleDragStart, false);
+      element.addEventListener('dragenter', handleDragEnter, false);
+      element.addEventListener('dragend', handleDragEnd, false);
+    });
   });
-});
 
-function handleDragEnter(dragEnterEvent) {
-  console.log('dragenter');
-}
+  function handleDragEnter(dragEnterEvent) {
+    console.log('dragenter');
+  }
 
-function handleDragLeave(dragLeaveEvent) {
-  console.log('dragleave');
+  function handleDragLeave(dragLeaveEvent) {
+    console.log('dragleave');
 
-  dragLeaveEvent.target.classList.remove('highlight');
-}
+    dragLeaveEvent.target.classList.remove('highlight');
+  }
 
-function unhighlight(element) {
-  element.classList.remove('highlight');
-}
+  function unhighlight(element) {
+    element.classList.remove('highlight');
+  }
 
-function handleDrop(dropEvent) {
-  console.log('drop');
-    
-  var data = dropEvent.dataTransfer.getData('text/plain');
-  var currentSum = dropEvent.target.innerHTML.replace('£', '');
-  var sum = parseInt(currentSum, 10) + parseInt(data, 10);
+  function handleDrop(dropEvent) {
+    console.log('drop');
 
-  dropEvent.target.innerHTML = '£' + sum;
+    var data = dropEvent.dataTransfer.getData('text/plain');
+    var currentSum = dropEvent.target.innerHTML.replace('£', '');
+    var sum = parseInt(currentSum, 10) + parseInt(data, 10);
 
-  unhighlight(dropEvent.target);
-  
-  dropEvent.preventDefault();
-}
+    dropEvent.target.innerHTML = '£' + sum;
 
-function handleDragStart(dragStartEvent) {
-  console.log('dragstart');
+    unhighlight(dropEvent.target);
 
-  var price = dragStartEvent.target.getAttribute('data-price');
-    
-  dragStartEvent.dataTransfer.setData('text/plain', price);
-  dragStartEvent.target.classList.add('dragging');
-}
+    dropEvent.preventDefault();
+  }
 
-function handleDragEnd(dragEndEvent) {
-  console.log('dragend');
-  
-  dragEndEvent.target.classList.remove('dragging');
-}
+  function handleDragStart(dragStartEvent) {
+    console.log('dragstart');
 
-function handleDragOver(dragOverEvent) {
-  console.log('dragover');
-  
-  dragOverEvent.target.classList.add('highlight');
+    var price = dragStartEvent.target.getAttribute('data-price');
 
-  dragOverEvent.stopPropagation();
-  dragOverEvent.preventDefault();
-}
+    dragStartEvent.dataTransfer.setData('text/plain', price);
+    dragStartEvent.target.classList.add('dragging');
+  }
+
+  function handleDragEnd(dragEndEvent) {
+    console.log('dragend');
+
+    dragEndEvent.target.classList.remove('dragging');
+  }
+
+  function handleDragOver(dragOverEvent) {
+    console.log('dragover');
+
+    dragOverEvent.target.classList.add('highlight');
+
+    dragOverEvent.stopPropagation();
+    dragOverEvent.preventDefault();
+  }
+
+})();
